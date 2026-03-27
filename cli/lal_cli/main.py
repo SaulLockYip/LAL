979fff3ac51e7713e7fba25cb064cbf65e95634f
Char Siu <stirdes@gmail.com>
2026-03-27 12:37:53 +0800

    Bump version to 0.2.0

--- cli/lal_cli/main.py
+++ cli/lal_cli/main.py
@@ -28,7 +28,7 @@ _running_processes: List[subprocess.Popen] = []
 
 
 @click.group()
-@click.version_option(version="0.1.0")
+@click.version_option(version="0.2.0")
 def cli() -> None:
     """LAL CLI - Learn Any Language CLI Tool.
 
